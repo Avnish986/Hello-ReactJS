f857fe3375f4ae99fec37d14708bc2508f9fd40f
avnish986 <avnishgupta986@gmail.com>
2020-11-29 22:52:16 +0530

    [Avnish]Added: dynamically displaying input message

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import logo from './logo.svg';
+import './App.css';
+import App2 from './App2'
+import React from "react";
+
+class App extends React.Component{
+  url='http://google.com'
+  constructor(){
+    super()
+    this.state= {
+      title: 'Hello World Constructor',
+      userName: ''
+    }
+  }
+
+  //onClick function
+  onClick=($event)=>{
+    console.log("Save Button is clicked:", $event);
+    window.open(this.url , "_blank");
+  }
+
+  //onName function
+  onNameChange=(event)=>{
+console.log("Value is",event.target.value)
+this.setState({userName: event.target.value})
+  }
+
+  render(){
+    return <div>
+      <div>
+      <h1>Hello {this.state.userName} and Welcome to ReactDemo</h1>
+   <img src={logo} onClick={this.onClick}
+   alt="Logo" width="300px"/>
+      </div>
+      <div className="text-box">
+        <input onChange={this.onNameChange}/>
+      </div>
+    </div>
+  }
+}
+
+
+export default App;
